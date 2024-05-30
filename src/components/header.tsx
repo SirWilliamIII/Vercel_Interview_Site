6cfd391454f1dd011019a1c8399fd009db877a85
Will Carpenter <willcarpenterwastaken@gmail.com>
2024-05-30 09:06:10 -0500

    changed landing page to interview questions page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="w-full absolute text-white z-10">
       <nav className="container relative flex flex-wrap items-center justify-between mx-auto">
-        <Link href="/">Landing</Link>
+        <Link href="/">Interview Questions</Link>
         <div className="space-x-4 text-xl">
           <Link href="/aboutme">About Me</Link>
           <Link href="/resume">Resume</Link>
