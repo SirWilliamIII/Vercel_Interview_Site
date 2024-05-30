379216042a8895a90b3a3d7df79b05e65967cbe0
Will Carpenter <willcarpenterwastaken@gmail.com>
2024-05-30 09:57:30 -0500

    removed links to ogther pages

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,12 +5,6 @@ export default function Header() {
     <div className="w-full absolute text-white z-10">
       <nav className="container relative flex flex-wrap items-center justify-between mx-auto">
         <Link href="/">Interview Questions</Link>
-        <div className="space-x-4 text-xl">
-          <Link href="/aboutme">About Me</Link>
-          <Link href="/resume">Resume</Link>
-          <Link href="/family">Family</Link>
-          <Link href="/hobbies">Hobbies</Link>
-        </div>
       </nav>
     </div>
   );
